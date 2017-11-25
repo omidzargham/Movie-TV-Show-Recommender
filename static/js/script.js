4e8db228e65a220540ad8cbfd6e57b79b531b6ae
Omid Afshar <omidafshar@berkeley.edu>
2017-11-24 17:08:35 -0800

    Fixed file paths in html and tested local storage setting

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+function setMovie() {
+
+    localStorage.setItem('entertainment_type', 'movie')
+
+}
+
+
+
+function setTV() {
+
+    localStorage.setItem('entertainment_type', 'tv')
+
+}
--- webserver.py
+++ webserver.py
@@ -12,29 +12,36 @@ import requests
 # "static folder"
 app = Flask(__name__,static_url_path="/static")
 
+base_url= "https://api.themoviedb.org/3/discover/movie?api_key=" + os.environ['API_KEY'] + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1"
+
+entertainment_type = "" # save this in local storage on browser
+
 @app.route("/")
-def hello_world():
+def home_page():
     """
     If someone goes to the root of your website (i.e. http://localhost:5000/), run this function. The string that this
     returns will be sent to the browser
     """
     return render_template("index.html") # Render the template located in "templates/index.html"
 
-@app.route("/about")
-def about():
-	return render_template("about.html")
-
-@app.route("/index")
-def index():
-	return render_template("index.html")
-
 @app.route("/movie")
 def movie():
-	return render_template("movie.html")
+    entertainment_type = "movie"
+    return render_template("movie.html")
 
 @app.route("/tv")
 def tv():
-	return render_template("tv.html")
+    entertainment_type = "tv"
+    return render_template("tv.html")
+
+@app.route("/list", methods=['POST'])
+def generate_list():
+    genre = "action"
+    discover_url = base_url + "&with_genre=" + genre
+    r = requests.get(discover_url)
+    print(r.json())
+    return render_template("movie.html") #should be list.html
+
 	
 
 if __name__ == "__main__":
